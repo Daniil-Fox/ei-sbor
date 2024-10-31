c65eb0563c9cf3a4bdfbd8addc3c8454aaf2dcc2
Artyushenko Daniil <foxones80@gmail.com>
2024-11-01 00:22:28 +0600

    fixed some

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,20 @@ window.addEventListener('DOMContentLoaded', e => {
             nextEl: '.proj-next',
         }
     })
+
+
+    const menuBtn = document.querySelector('.header__burger')
+    const menu = document.querySelector('.menu')
+    const menuItems = menu.querySelectorAll('.nav__link')
+    menuBtn.addEventListener('click', e => {
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+    })
+
+    menuItems.forEach(link => {
+        link.addEventListener('click', e => {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+        })
+    })
 })
